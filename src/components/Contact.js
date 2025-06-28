5a49d14ed3cbe58ab3def55b76cf7d7c1281a74c
Chris <mullerchristophe21@gmail.com>
2025-06-28 16:48:55 +0200

    email address hidden

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,7 @@ function Contact() {
       <h2>Get In Touch</h2>
       <p>I'm always open to new opportunities and collaborations. Feel free to reach out!</p>
       <div className="contact-info">
-        <p>Email: <a href="mailto:mullerchristophe21@gmail.com">mullerchristophe21@gmail.com</a></p>
+        <p>Email: mullerchristophe21[~at~]gmail[~dot~]com</p>
         <p>LinkedIn: <a href="https://www.linkedin.com/in/christophe-muller" target="_blank" rel="noopener noreferrer">linkedin.com/in/christophe-muller</a></p>
         <p>GitHub: <a href="https://github.com/ChristopheMuller" target="_blank" rel="noopener noreferrer">github.com/ChristopheMuller</a></p>
         {/* You can add a simple form later if you wish */}
