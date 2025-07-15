e9bb9fe8b92536705a8dabd3b501b6a8a8f4cb02
Chris <mullerchristophe21@gmail.com>
2025-07-15 21:08:54 +0200

    undo weird google ref thing

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Journey from './components/Journey';
 import Contact from './components/Contact';
-// import GameOfLife from './components/GameOfLife';
-// import KMeansVisualizer from './components/KMeansVisualizer';
-// import RegressionFitter from './components/RegressionFitter';
-// import GoogleSheetEmbed from './components/GoogleSheetEmbed'; // If you also have the full sheet embed
 import Research from './components/Research';
 import Footer from './components/Footer';
 
