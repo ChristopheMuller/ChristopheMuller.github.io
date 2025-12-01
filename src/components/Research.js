abee91b6b86a501efa49c690893671dda7ce5cfc
Christophe Muller <mullerchristophe21@gmail.com>
2025-12-01 21:24:08 +0000

    fix: Update research paper links to DOI

--- src/components/Research.js
+++ src/components/Research.js
@@ -7,8 +7,8 @@ function Research() {
       id: 2,
       title: 'Do we Need Dozens of Methods for Real World Missing Value Imputation?',
       authors: 'K. Grzesiak, C. Muller, J. Josse, J. Näf',
-      venue: 'Preprint on Arxiv',
-      link: 'https://arxiv.org/abs/2511.04833',
+      venue: 'Preprint on Arxiv.',
+      link: 'https://doi.org/10.48550/arXiv.2511.04833',
       stamp: 'Preprint',
       repo: 'https://github.com/ChristopheMuller/benchmark'
     },
@@ -16,8 +16,8 @@ function Research() {
       id: 1,
       title: 'Graph-based multi-agent reinforcement learning for railway infrastructure decision support',
       authors: 'G. Arcieri, G. Duthé, C. Muller, D. Haener, K. Papakonstantinou, D. Straub, E. Chatzi',
-      venue: '14th International Conference on Structural Safety and Reliability - ICOSSAR25',
-      link: 'https://www.scipedia.com/public/Arcieri_et_al_2025a',
+      venue: '14th International Conference on Structural Safety and Reliability - ICOSSAR25.',
+      link: 'https://doi.org/10.23967/icossar.2025.118',
       stamp: 'Conference',
       repo: 'https://github.com/ChristopheMuller/MARL_optimal_maintenance_graph_railway_network'
     },
@@ -25,8 +25,8 @@ function Research() {
       id: 2,
       title: 'When Pattern-by-Pattern Works: Theoretical and Empirical Insights for Logistic Models with Missing Values',
       authors: 'C. Muller, E. Scornet, J. Josse',
-      venue: 'Preprint on Arxiv',
-      link: 'https://arxiv.org/abs/2507.13024',
+      venue: 'Preprint on Arxiv.',
+      link: 'https://doi.org/10.48550/arXiv.2507.13024',
       stamp: 'Preprint',
       repo: 'https://github.com/ChristopheMuller/logistic_with_NAs'
     },
