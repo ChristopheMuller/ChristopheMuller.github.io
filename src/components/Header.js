581eb8a2def74c4a419190269ffba0b26b077189
Christophe Muller <mullerchristophe21@gmail.com>
2025-12-02 18:47:19 +0000

    update cv link

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ function Header() {
           <li><a href="#about" onClick={() => setMenuOpen(false)}>About</a></li>
           <li><a href="#journey" onClick={() => setMenuOpen(false)}>Journey</a></li>
           <li><a href="#research" onClick={() => setMenuOpen(false)}>Research</a></li>
-          <li><a href="https://drive.google.com/file/d/1kX6Ipb34hY47925-Hjmurl4MtMpvPmlz/view?usp=sharing" target="_blank" rel="noopener noreferrer">CV</a></li>
+          <li><a href="https://drive.google.com/file/d/1yv1W7eKhy0TpWrzRbeUlMVBZvCY3nFib/view?usp=sharing" target="_blank" rel="noopener noreferrer">CV</a></li>
           <li><a href="#contact" onClick={() => setMenuOpen(false)}>Contact</a></li>
         </ul>
       </nav>
