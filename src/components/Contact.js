ac180ec99365826c70cb4f3d42d85034eb19a46b
Christophe Muller <mullerchristophe21@gmail.com>
2025-12-06 16:51:04 +0000

    feat: Remove GameOfLife and Skills components, and update contact email.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,7 @@ function Contact() {
       <h2>Get In Touch</h2>
       <p>I'm always open to new opportunities and collaborations. Feel free to reach out!</p>
       <div className="contact-info">
-        <p>Email: mullerchristophe21[~at~]gmail[~dot~]com</p>
+        <p>Email: christophe.muller@stats.ox.ac.uk</p>
         <p>LinkedIn: <a href="https://www.linkedin.com/in/christophe-muller" target="_blank" rel="noopener noreferrer">linkedin.com/in/christophe-muller</a></p>
         <p>GitHub: <a href="https://github.com/ChristopheMuller" target="_blank" rel="noopener noreferrer">github.com/ChristopheMuller</a></p>
         <p>CV: <a href="https://drive.google.com/file/d/1yv1W7eKhy0TpWrzRbeUlMVBZvCY3nFib/view?usp=sharing" target="_blank" rel="noopener noreferrer">Download CV</a></p>
